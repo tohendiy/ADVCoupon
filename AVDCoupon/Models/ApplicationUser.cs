11629e615176e180ddec48da7cae607fdd4c06ce
olecsiuyae <olecsiuyae@gmail.com>
2018-04-12 11:36:45 +0300

    Added new entities

--- AVDCoupon/Models/ApplicationUser.cs
+++ AVDCoupon/Models/ApplicationUser.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreEntities;
 using Microsoft.AspNetCore.Identity;
 
 namespace AVDCoupon.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<Guid>
     {
+        public List<Coupon> UserCoupons { get; set; }
+
+        // statistics data
+        
+        public DateTime Birthday { get; set; }
+        public string City { get; set; }
+        public int ChildNumber { get; set; }
     }
 }
---
+++ AVDCoupon/Models/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVDCoupon.Models
+{
+    public class Coupon
+    {
+        public Guid CouponGuid { get; set; }
+        public int Capacity { get; set; }
+        public string CouponImage { get; set; }
+        public string CouponName { get; set; }
+        public ApplicationUser MerchantUser { get; set; }
+    }
+}
