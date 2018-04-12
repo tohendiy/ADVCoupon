6f9ff97b1f43dd4db9cf33f00e35c595247efb44
olecsiuyae <olecsiuyae@gmail.com>
2018-04-12 15:53:38 +0300

    Small changes

--- AVDCoupon/Models/ApplicationUser.cs
+++ AVDCoupon/Models/ApplicationUser.cs
@@ -9,7 +9,6 @@ namespace AVDCoupon.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-
         public List<Coupon> UserCoupons { get; set; }
 
         // statistics data
