060ede3bc102c6d27cce26bb4b5189077e6d4e6a
Anton <tohendiy@gmail.com>
2018-04-13 13:31:50 +0300

    Added missing ViewModels

---
+++ AVDCoupon/ViewModel/CouponViewModel/CouponEditItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADVCoupon.ViewModel.CouponViewModel
+{
+    public class CouponEditItemViewModel
+    {
+        public Guid CouponGuid { get; set; }
+
+        public int TotalCapacity { get; set; }
+
+        public int CurrentCapacity { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string CouponImage { get; set; }
+
+        [Required]
+        public string CouponName { get; set; }
+
+    }
+}
