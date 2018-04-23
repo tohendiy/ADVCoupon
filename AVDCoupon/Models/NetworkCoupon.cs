01c1a547a5d8efa7096342163881aaf8887dbed9
Anton <tohendiy@gmail.com>
2018-04-23 10:59:36 +0300

    Added Many-to-Many model

---
+++ AVDCoupon/Models/NetworkCoupon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AVDCoupon.Models;
+
+namespace ADVCoupon.Models
+{
+    public class NetworkCoupon
+    {
+        public Guid NetworkId { get; set; }
+
+        public Network Network { get; set; }
+
+        public Guid CouponId { get; set; }
+
+        public Coupon Coupon { get; set; }
+    }
+}
