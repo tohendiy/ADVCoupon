bf68509186646b009f7cf3347a7f887dd17fc975
olecsiuyae <olecsiuyae@gmail.com>
2018-04-23 17:23:19 +0300

    Migration

--- AVDCoupon/Migrations/20180423142140_FixErrorInCity.Designer.cs
+++ AVDCoupon/Migrations/20180423142140_FixErrorInCity.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace ADVCoupon.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180423142140_FixErrorInCity")]
+    partial class FixErrorInCity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AVDCoupon/Migrations/20180423142140_FixErrorInCity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ADVCoupon.Migrations
+{
+    public partial class FixErrorInCity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cuty",
+                table: "Geopositions",
+                newName: "City");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "Geopositions",
+                newName: "Cuty");
+        }
+    }
+}
