b235153dcef508502a55b066e26f47e8cde97018
Anton <tohendiy@gmail.com>
2018-05-07 17:42:56 +0300

    Continue Mail sender flow
    Continue Mail sender flow

--- AVDCoupon/Services/Interfaces/IEmailSender.cs
+++ AVDCoupon/Services/Interfaces/IEmailSender.cs
@@ -8,5 +8,7 @@ namespace AVDCoupon.Services
     public interface IEmailSender
     {
         Task SendEmailAsync(string email, string subject, string message);
+
+        Task PostMessage(List<string> role, string message, string subject);
     }
 }
---
+++ AVDCoupon/ViewModel/MailViewModels/MailViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ADVCoupon.ViewModel.MailViewModels
+{
+    public class MailViewModel
+    {
+        [Display(Name ="Тема")]
+        [Required]
+        public string Subject { get; set; }
+
+        [Display(Name = "Сообщение")]
+        [Required]
+        public string Message { get; set; }
+
+        [Display(Name = "Получатели")]
+        public MultiSelectList RecipientsRoles { get; set; }
+
+        [Display(Name = "Получатели")]
+        [Required]
+        public IEnumerable<string> Roles { get; set; }
+    }
+}
